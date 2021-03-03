3c9f92a3c97b3d53ec3fac04f3160d976fdd3992
Lrd134 <larryc3200@gmail.com>
2021-03-03 14:45:56 -0500

    commit tree 2dc903612a18d73af9942214b070258e85f1a8bd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    l = array.size
+    if l < 2
+        return array.join
+    elsif l < 3
+        return array.join(" and ")
+    end
+    array[l-1] = "and #{array[l-1]}"
+    array.join(", ")
 end
